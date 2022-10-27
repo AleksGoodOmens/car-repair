9ab3622867b4691fa97ef02dacb9b090b0281ca8
Aleksei <agsfinks@gmail.com>
2022-10-27 23:01:47 +0100

    Add data base

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('content is loaded');
     // const tab = document.querySelector('.gallery__navigation-tabs');
-    // console.log(tabs);
     const tabs = document.querySelectorAll('.gallery__navigation-tab');
-    console.log(tabs);
     const slides = document.querySelectorAll('.gallery__slide');
-    console.log(slides);
     const galleryPictures = document.querySelectorAll('.gallery-pictures__item');
 
     tabs.forEach((tab, index) => {
@@ -29,7 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // });
 
     function showSlide(element, classes = 'active') {
-        console.log('show');
         element.classList.add(classes);
 
     }
@@ -42,7 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
     }
-
 
     class Card {
         constructor(comment, src, name, parentSelector, ...classes) {
@@ -68,24 +62,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new Card(
-        'I used the services of this vervis and was satisfied! I can recommend these guys for solving the most difficult tasks!',
-        "https://source.unsplash.com/random/200x200?sig=16",
-        'Aleks Gomeniuk',
-        '.reviews-wrapper',
-
-    ).renderNewCard();
-    new Card(
-        'Я очень рада оставить свой комментарий а данной страничке.',
-        "https://source.unsplash.com/random/200x200?sig=44",
-        'Valentina Gomeniuk',
-        '.reviews-wrapper'
-    ).renderNewCard();
-
     class AutoCardRender {
-        constructor(parentSelector, cardClass, imgBoxClass, imgClass, imgSrc, imgAlt, textBoxClass, titleClass, title, textClass, comment, ...moreClasses) {
+        constructor(parentSelector, cardClass, imgBoxClass, imgClass, imgSrc, imgAlt, textBoxClass, titleClass, title, textClass, comment) {
             this.parentSelector = parentSelector;
-            this.parent = document.querySelector(parentSelector);
             this.cardClass = cardClass;
             this.imgBoxClass = imgBoxClass;
             this.imgClass = imgClass;
@@ -96,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
             this.title = title;
             this.textClass = textClass;
             this.comment = comment;
-            this.moreClasses = moreClasses;
+            this.parent = document.querySelector(parentSelector);
         }
 
         renderCard() {
@@ -108,147 +87,103 @@ document.addEventListener('DOMContentLoaded', () => {
                 <div class=${this.textClass}>${this.comment}</div>
             </div>`;
             this.parent.append(div);
-            console.log(`<div class=${this.titleClass + ' ' + this.moreClasses}>${this.title}</div>`);
         }
     }
 
-    new AutoCardRender(
-        '.promise__cards',
-        'promise__card',
-        'promise__img-box',
-        'promise__img',
-        './imgs/section-promise/1.png',
-        'icon promise',
-        'promise__text-box',
-        'class="promise__card-title text-darkBlue"',
-        'Quality service. Everytime.',
-        'promise__text',
-        'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium corrupti quos dolores et quas.',
-        ''
-    ).renderCard();
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-    new AutoCardRender(
-        '.promise__cards',
-        'promise__card',
-        'promise__img-box',
-        'promise__img',
-        './imgs/section-promise/2.png',
-        'icon promise',
-        'promise__text-box',
-        'class="promise__card-title text-darkBlue"',
-        'Services by certified pros',
-        'promise__text',
-        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, in! Sequi dolorem sit consequatur a!',
-        'text-darkBlue',
-    ).renderCard();
+        if (!res.ok) {
+            throw new Error(`Cloud not fetch ${url}, status: ${res.status}`);
+        }
 
-    new AutoCardRender(
-        '.promise__cards',
-        'promise__card',
-        'promise__img-box',
-        'promise__img',
-        './imgs/section-promise/3.png',
-        'icon promise',
-        'promise__text-box',
-        'class="promise__card-title text-darkBlue"',
-        'Real people. Honest work.',  //!
-        'promise__text',
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi atque ut quas maiores vero tempore similique quo praesentium itaque eius dolor animi incidunt, possimus ipsam perferendis amet, facere hic quidem?',
-        'text-darkBlue',
-    ).renderCard();
+        return await res.json();
+    };
+
+    getResource('http://localhost:3000/customersCards')
+        .then(data => {
+            data.forEach(({ comment, img, name, parentClass }) => {
+                new Card(comment, img, name, parentClass).renderNewCard();
+            });
+        });
+
+    getResource('http://localhost:3000/promiseCards')
+        .then(data => {
+            data.forEach(({ parentSelector, cardClass, imgBoxClass, imgClass, imgSrc, imgAlt, textBoxClass, titleClass, title, textClass, comment }) => {
+                new AutoCardRender(parentSelector, cardClass, imgBoxClass, imgClass, imgSrc, imgAlt, textBoxClass, titleClass, title, textClass, comment).renderCard();
+            });
+        });
+
+    getResource('http://localhost:3000/serviceCards')
+        .then(data => {
+            data.forEach(({ parentSelector, cardClass, imgBoxClass, imgClass, imgSrc, imgAlt, textBoxClass, titleClass, title, textClass, comment }) => {
+                new AutoCardRender(parentSelector, cardClass, imgBoxClass, imgClass, imgSrc, imgAlt, textBoxClass, titleClass, title, textClass, comment).renderCard();
+            });
+        });
+    //! send
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Thank you!',
+        failure: 'Error'
+    };
+
+    forms.forEach(item => {
+        bindPostData(item);
+    });
+
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            form[5].firstElementChild.textContent = message.loading;
+
+            const formData = new FormData(form);
+
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
 
-    //service
-    new AutoCardRender(
-        '.service__cards',
-        'service__card',
-        'service__img-box',
-        'service__img',
-        `'./imgs/section-service/1.png'`, //!
-        'service Icon',
-        'service__text-box',
-        'class="service__title"',
-        'Multi Point Inspections', //!
-        'service__description',
-        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia delectus officia eius minima temporibus, blanditiis esse fuga debitis.',  //!
+            postData('http://localhost:3000/requests', JSON.stringify(json)
+                .then(data => {
+                    console.log(data);
+                    form[5].classList.add('greenBG');
+                    form[5].firstElementChild.textContent = message.success;
+                }).catch(() => {
+                    form[5].firstElementChild.textContent.textContent = message.failure;
+                }).finally(() => {
+                    setTimeout(() => {
+                        form.reset();
+                        form[5].classList.remove('greenBG');
+                        form[5].firstElementChild.textContent = 'Make an appointment';
+                    }, 4000);
+                }));
+        });
+    }
 
-    ).renderCard();
+    //? get Data
+    //? fetch
+    fetch('http://localhost:3000/customersCards')
+        .then(data => data.json())
+        .then(res => console.log(res));
 
-    new AutoCardRender(
-        '.service__cards',
-        'service__card',
-        'service__img-box',
-        'service__img',
-        `'./imgs/section-service/2.png'`, //!
-        'service Icon',
-        'service__text-box',
-        'class="service__title"',
-        'Auto Repair', //!
-        'service__description',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad hic sapiente quaerat deleniti in ipsa pariatur explicabo accusantium fugit! Totam?',  //!
+    fetch('http://localhost:3000/serviceCards')
+        .then(data => data.json())
+        .then(res => console.log(res));
 
-    ).renderCard();
-
-    new AutoCardRender(
-        '.service__cards',
-        'service__card',
-        'service__img-box',
-        'service__img',
-        `'./imgs/section-service/3.png'`, //!
-        'service Icon',
-        'service__text-box',
-        'class="service__title"',
-        'Auto Maintenance', //!
-        'service__description',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nam voluptates odit nostrum, aspernatur est.',  //!
-
-    ).renderCard();
-
-    new AutoCardRender(
-        '.service__cards',
-        'service__card',
-        'service__img-box',
-        'service__img',
-        `'./imgs/section-service/4.png'`, //!
-        'service Icon',
-        'service__text-box',
-        'class="service__title"',
-        'Tires', //!
-        'service__description',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nam voluptates odit nostrum, aspernatur est.',  //!
-
-    ).renderCard();
-
-    new AutoCardRender(
-        '.service__cards',
-        'service__card',
-        'service__img-box',
-        'service__img',
-        `'./imgs/section-service/5.png'`, //!
-        'service Icon',
-        'service__text-box',
-        'class="service__title"',
-        'Service Warranty', //!
-        'service__description',
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, possimus?', //!
-
-    ).renderCard();
-
-    new AutoCardRender(
-        '.service__cards',
-        'service__card',
-        'service__img-box',
-        'service__img',
-        `'./imgs/section-service/6.png'`, //!
-        'service Icon',
-        'service__text-box',
-        'class="service__title"',
-        'Shop Online', //!
-        'service__description',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ad dolor repudiandae? Mollitia, quas unde? Ducimus dolor nihil distinctio maxime.',  //!
-
-    ).renderCard();
-
+    fetch('http://localhost:3000/promiseCards')
+        .then(data => data.json())
+        .then(res => console.log(res));
 
 });
 
